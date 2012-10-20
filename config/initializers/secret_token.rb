14ca65381bcac0e84dc59dbcf385b6a8b64c1d03
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-10-20 23:44:06 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kpty::Application.config.secret_token = '966473bdaf96f0b88a797ef13f3da874d7aefd80633b2b831aff08247bd3011ba0414c1617ca09c6b2d906034fca9db4b7e7fd72aee24d9cfe4a0d54b016ad87'
