590598a26bf093aa04f526fb6113eb6122ca6593
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-11-05 22:10:22 +0900

    shape up controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,79 +2,43 @@ class ProjectsController < ApplicationController
 
   before_filter :find_project, :only => [:show, :edit, :update, :destroy]
 
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = current_user.projects
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @projects }
-    end
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @project }
-    end
   end
 
-  # GET /projects/new
-  # GET /projects/new.json
   def new
     @project = current_user.projects.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
   end
 
-  # GET /projects/1/edit
   def edit
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = current_user.projects.create(params[:project].merge(:creator_id => current_user.id))
 
-    respond_to do |format|
-      if @project.valid?
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.valid?
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PUT /projects/1
-  # PUT /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.update_attributes(params[:project])
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @project.destroy
-
-    respond_to do |format|
-      format.html { redirect_to projects_path }
-    end
+    redirect_to projects_path
   end
+
 
   private
 
