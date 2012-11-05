c50cf132a42d8a81d1bc855726f5d3ee02f7fb33
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-11-05 21:41:35 +0900

    Set admin when create project.

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -3,4 +3,13 @@ class Affiliation < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :project
+
+  after_create :set_admin
+
+  private
+
+  def set_admin
+    return unless self.project.creator_id == self.user.id
+    self.update_attribute(:admin, true)
+  end
 end
