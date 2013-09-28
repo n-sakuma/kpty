2bc3d4c18b15a2cb06b4fad5dfa21d981292ce96
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-09-28 21:08:41 +0900

    Replace rails4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kpty::Application.config.secret_token = '966473bdaf96f0b88a797ef13f3da874d7aefd80633b2b831aff08247bd3011ba0414c1617ca09c6b2d906034fca9db4b7e7fd72aee24d9cfe4a0d54b016ad87'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kpty::Application.config.secret_key_base = '9b745d1127eaa33044066d9e69c7e791e62b1ce15c7be7a4f47913a97b697cbd60db0e5918873cb9a961a0adfa5649410a71d68a29e96669d04b32f01c88b8fb'
