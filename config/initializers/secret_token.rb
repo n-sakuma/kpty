2427290bb1a7f4df86493e49ea43174366c94e14
Naohiro Sakuma <nao.bear@gmail.com>
2013-12-11 01:48:23 +0900

    update rails, 3.2.16

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kpty::Application.config.secret_token = '966473bdaf96f0b88a797ef13f3da874d7aefd80633b2b831aff08247bd3011ba0414c1617ca09c6b2d906034fca9db4b7e7fd72aee24d9cfe4a0d54b016ad87'
+Kpty::Application.config.secret_token = '9ff627b176e5b5fc394eeecea5f81f6c52059510d030c966ea2edf94def722e5daa837b5de612ba8e42f04ecfb1fef0489ba72be65addfe3587c1a3cba7ebb58'
